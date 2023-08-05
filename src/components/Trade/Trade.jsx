65f2326e3b8afbffe8c75b0bfd1604b8389e8ad5
Ivan Gomyranov <ffreedom37@gmail.com>
2023-08-05 14:56:35 +0300

    makeupFunctionalPanel

--- src/components/Trade/Trade.jsx
+++ src/components/Trade/Trade.jsx
@@ -1,10 +1,12 @@
 import PlaceToGive from "./components/PlaceToGive/PlaceToGive"
 import PlaceToGet from "./components/PlaceToGet/PlaceToGet"
+import FunctionalPanel from "./components/FunctionalPanel/FunctionalPanel"
 
 export default function Trade() {
     return (
         <div className="trade">
         <PlaceToGive />
+        <FunctionalPanel />
         <PlaceToGet />
         </div>
     )
---
+++ src/components/Trade/components/FunctionalPanel/FunctionalPanel.jsx
@@ -0,0 +1,7 @@
+export default function FunctionalPanel() {
+    return(
+        <section className="functional-panel">
+            <button className="functional-panel__submit-trade"><p className="functional-panel__submit-trade-caption">ОБМЕН</p></button>
+        </section>
+    )
+}
