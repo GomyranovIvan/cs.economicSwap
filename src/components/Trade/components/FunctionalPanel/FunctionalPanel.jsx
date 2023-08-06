4c6b03e5c0712dda48b314bd5bcf8cea61193e9d
Ivan Gomyranov <ffreedom37@gmail.com>
2023-08-06 10:09:14 +0300

    makeupFunctionalPanel

--- src/components/Trade/components/FunctionalPanel/FunctionalPanel.jsx
+++ src/components/Trade/components/FunctionalPanel/FunctionalPanel.jsx
@@ -1,7 +1,18 @@
 export default function FunctionalPanel() {
-    return(
+    return (
         <section className="functional-panel">
-            <button className="functional-panel__submit-trade"><p className="functional-panel__submit-trade-caption">ОБМЕН</p></button>
+            <button className="functional-panel__submit-trade">
+                <p className="functional-panel__submit-trade-caption">ОБМЕН</p>
+            </button>
+            <div className="functional-panel__collapse-header-price">
+                <p className="functional-panel__display-price">Цена</p>
+                <button className="button__collapse"></button>
+            </div>
+            <div className="functional-panel__container-input">
+                <input className="functional-panel__input-price-start" placeholder="$ 0,00"></input>
+                <div className="functional-panel__dash"></div>
+                <input className="functional-panel__input-price-end" placeholder="$ 0,00"></input>
+            </div>
         </section>
     )
 }
