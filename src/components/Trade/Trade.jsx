9015cf68df5b77adeba1c84cac73289ec24693ca
Ivan Gomyranov <ffreedom37@gmail.com>
2023-08-07 01:45:49 +0300

    active trade place

--- src/components/Trade/Trade.jsx
+++ src/components/Trade/Trade.jsx
@@ -1,13 +1,49 @@
 import PlaceToGive from "./components/PlaceToGive/PlaceToGive"
 import PlaceToGet from "./components/PlaceToGet/PlaceToGet"
 import FunctionalPanel from "./components/FunctionalPanel/FunctionalPanel"
+import basket from "../../images/TradeBasket.svg"
+import { useState } from "react"
 
 export default function Trade() {
+
+    const [placeToGiveActive, setPlaceToGiveActive] = useState(false)
+    const [placeToGetActive, setPlaceToGetActive] = useState(false)
+
     return (
         <div className="trade">
-        <PlaceToGive />
-        <FunctionalPanel />
-        <PlaceToGet />
+            <section className="place-give">
+                <div className="place-give__header" onClick={() => setPlaceToGiveActive(!placeToGiveActive)}>
+                    <button className="place-give__button-window-item">
+                    </button>
+                    <h3 className="place-give__title">ВЫ ОТДАДИТЕ</h3>
+                    <div className="place-give__sum-container">
+                        <p className="place-give__sum">$ 0,00</p>
+                        <img
+                            src={basket}
+                            className="place-give__basket"
+                            alt="Корзина"
+                        />
+                    </div>
+                </div>
+                <PlaceToGive active={placeToGiveActive} />
+            </section>
+            <FunctionalPanel />
+            <section className="place-get">
+                <div className="place-get__header" onClick={() => setPlaceToGetActive(!placeToGetActive)}>
+                    <div className="place-get__sum-container">
+                        <img
+                            src={basket}
+                            className="place-get__basket"
+                            alt="Корзина"
+                        />
+                        <p className="place-get__sum">0,00 $</p>
+                    </div>
+                    <h3 className="place-get__title">ВЫ ПОЛУЧИТЕ</h3>
+                    <button className="place-get__button-window-item">
+                    </button>
+                </div>
+                <PlaceToGet active={placeToGetActive} />
+            </section>
         </div>
     )
 }
--- src/components/Trade/components/PlaceToGive/PlaceToGive.jsx
+++ src/components/Trade/components/PlaceToGive/PlaceToGive.jsx
@@ -1,23 +1,12 @@
-import basket from "../../../../images/TradeBasket.svg"
-export default function PlaceToGive() {
+import React from "react"
+export default function PlaceToGive({ active }) {
     return (
-        <section className="place-give">
-            <div className="place-give__header">
-                <button className="place-give__button-window-item">
-                </button>
-                <h3 className="place-give__title">ВЫ ОТДАДИТЕ</h3>
-                <div className="place-give__sum-container">
-                <p className="place-give__sum">$ 0,00</p>
-                <img
-                src={basket}
-                className="place-give__basket"
-                alt="Корзина"
-                />
-                </div>
-            </div>
+        <>
+            {active && (
             <div className="place-give__main">
-                <p className="place-give__text-placeholder">Добавьте предметы, которые хотите обменять</p>
+                <p className="place-give__text-placeholder" >Добавьте предметы, которые хотите обменять</p>
             </div>
-        </section>
+            )}
+        </>
     )
 }
